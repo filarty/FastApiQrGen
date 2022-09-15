1b1fe1ebe629afa81a23b12c36e05f617d512767
filarty <filarty@gmail.com>
2022-09-15 19:11:32 +0200

    add requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-hello
+from operator import mod
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Model(BaseModel):
+    status: int
+    message: str
+
+    
+app = FastAPI()
+
+
+@app.post("/qr")
+async def index(model: Model):
+    return model
